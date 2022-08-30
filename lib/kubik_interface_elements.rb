208d397fbd1baf8dbee930128eeef6260f395f0a
Bart O <bart@primate.co.uk>
2022-08-30 15:15:50 +0100

    removed gallery references

--- lib/kubik_interface_elements.rb
+++ lib/kubik_interface_elements.rb
@@ -6,7 +6,7 @@ module KubikInterfaceElements
       isolate_namespace KubikInterfaceElements
 
       config.assets.precompile += %w( kubik_interface_elements.js )
-      initializer :kubik_media_library do
+      initializer :kubik_interface_elements do
         ActiveAdmin.application.load_paths += Dir[File.dirname(__FILE__) + '/arbre']
       end
     end
