7dccef78124225068df14933d7cae98f6b09a54f
cfbonner <cfbonner@gmail.com>
2022-10-24 12:01:34 +0100

    Remove text from template

--- package/src/controllers/modal_controller.ts
+++ package/src/controllers/modal_controller.ts
@@ -38,7 +38,7 @@ export default class extends Controller {
           <div class='kubik-modal-background' data-action='click->kubik-modal#closeModal'>
         </div>
       <div class='kubik-modal-element-window'>
-        <div data-kubik-modal-target='modalHeader' class='h4 kubik-modal-title-bar'>Test header</div>
+        <div data-kubik-modal-target='modalHeader' class='h4 kubik-modal-title-bar'></div>
         <turbo-frame id='media_library_frame' class='kubik-modal-element-content' data-kubik-modal-target='modalFrame'>
         </turbo-frame>
       </div>
--- package/src/templates/modal.ts
+++ package/src/templates/modal.ts
@@ -4,7 +4,7 @@ export default `
       <div class='kubik-modal-background' data-action='click->kubik-modal#closeModal'>
       </div>
       <div class='kubik-modal-element-window'>
-        <div data-kubik-modal-target='modalHeader' class='h4 kubik-modal-title-bar'>Test header</div>
+        <div data-kubik-modal-target='modalHeader' class='h4 kubik-modal-title-bar'></div>
         <turbo-frame id='media_library_frame' class='kubik-modal-element-content' data-kubik-modal-target='modalFrame'>
         </turbo-frame>
       </div>
